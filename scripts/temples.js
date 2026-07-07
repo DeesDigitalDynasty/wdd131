93ad8a175ee64ae5c16726a8affe41827ab75782
DeesDigitalDynasty <oluboyededayo@gmail.com>
2026-07-07 14:09:59 +0100

    Responsiveness  site for week 02

---
+++ scripts/temples.js
@@ -0,0 +1,25 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+
+        menuButton.textContent = "✕";
+
+    } else {
+
+        menuButton.textContent = "☰";
+
+    }
+
+});
+
+
+document.querySelector("#currentyear").textContent =
+new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modification: ${document.lastModified}`;
