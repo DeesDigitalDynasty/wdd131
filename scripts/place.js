fc3401419266a2a149690e96061b2c4f7c2c9349
DeesDigitalDynasty <oluboyededayo@gmail.com>
2026-07-16 19:50:12 +0100

    Lighthouse & Contrast issue fix

--- scripts/place.js
+++ scripts/place.js
@@ -16,14 +16,8 @@ const windChillElement = document.getElementById("windchill");
 
 // Wind Chill
 function calculateWindChill(temp, speed) {
-    return (
-        13.12 +
-        (0.6215 * temp) -
-        (11.37 * Math.pow(speed, 0.16)) +
-        (0.3965 * temp * Math.pow(speed, 0.16))
-    ).toFixed(1);
+    return (13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16))).toFixed(1);
 }
-
 
 // Only calculate when conditions are valid
 if (temperature <= 10 && windSpeed > 4.8) {
